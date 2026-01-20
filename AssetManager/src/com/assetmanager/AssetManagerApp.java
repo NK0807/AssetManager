eaeab700b288ccdd8bc1912f2b3bcfd70ba379d7
kow0807 <hongyue603@gmail.com>
2026-01-20 22:09:29 +0900

    Assetクラスの呼び出し処理を追加

--- AssetManager/src/com/assetmanager/AssetManagerApp.java
+++ AssetManager/src/com/assetmanager/AssetManagerApp.java
@@ -4,5 +4,13 @@ public class AssetManagerApp {
 	public static void main(String[] args) {
 		// 動作確認用
 		System.out.println("資産アプリを起動します。");
+		
+		// Asset(資産)を生成
+		Asset myMoney = new Asset("銀行A", 10000, "銀行");
+		
+		// 中身の確認
+		System.out.println("名前：" + myMoney.getName());
+		System.out.println("残高：" + myMoney.getBalance());
+		System.out.println("種類：" + myMoney.getType());
 	}
 }
